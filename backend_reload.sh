5279fc7875b143e9af4b20a550a61604dfdeffd4
drahmedjava <drahmedjava@gmail.com>
2024-09-01 14:35:31 +0400

    backup of client files as of 300724

--- backend_reload.sh
+++ backend_reload.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-kubectl delete -f backend-statefulset-scheduler.yaml 
-kubectl apply -f backend-statefulset-scheduler.yaml 
-kubectl delete -f backend-statefulset.yaml 
+kubectl delete -f backend-statefulset-scheduler.yaml
+kubectl apply -f backend-statefulset-scheduler.yaml
+kubectl delete -f backend-statefulset.yaml
 kubectl apply -f backend-statefulset.yaml
--- db_exec.sh
+++ db_exec.sh
@@ -1,6 +1,2 @@
 #! /bin/bash
 kubectl exec -it mongodb-statefulset-0 -- bash
-
-# mongosh -u 2135dac8bc8741f8  -p b7fe42219dcefc4a  --authenticationDatabase admin
-# use ueyedata_live;
-# ex: db.accounts.find({},{firstName: 1}).sort({createdAt: -1})[.limit(1)][.count()]
